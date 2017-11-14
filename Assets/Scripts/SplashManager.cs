ee4a46b9bc867ced85016c66850fe50c23cb3499
Ulisses Parola <ulissesmt@gmail.com>
2017-11-14 09:24:10 -0200

    Added some assets, create a splash screen and a start menu screen

---
+++ Assets/Scripts/SplashManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashManager : MonoBehaviour
+{
+	private float _sceneChangeTime;
+	public LevelManager LevelManager;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_sceneChangeTime = Time.time + 2.5f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= _sceneChangeTime)
+		{
+			LevelManager.LoadScene("StartMenu");
+			
+		}
+	}
+}
