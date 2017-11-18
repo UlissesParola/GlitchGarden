9e1b5f0d5809c0db02a5dd6d1b4043225d798e8a
Ulisses Parola <ulissesmt@gmail.com>
2017-11-17 23:08:59 -0200

    added fade in effect at the start menu scene

---
+++ Assets/Scripts/FadeInBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInBehaviour : MonoBehaviour
+{
+
+	public float FadeInTime;
+
+	private Image _fadeInPanel;
+	private Color _currentColor = Color.white;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_fadeInPanel = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.timeSinceLevelLoad < FadeInTime)
+		{
+			_currentColor.a = 1 - (Time.timeSinceLevelLoad / FadeInTime);
+			_fadeInPanel.color = _currentColor;
+
+		}
+		else
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
