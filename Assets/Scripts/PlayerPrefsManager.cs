07670c2ca4988b1595d53a7e2a2124e667175297
Ulisses Parola <ulissesmt@gmail.com>
2017-11-18 21:16:45 -0200

    create PlayerPrefsManager to (guess what) manager the PlayerPrefs

---
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsManager : MonoBehaviour
+{
+	private const string MasterVolumeKey = "MasterVolume";
+	private const string DifficultyKey = "Difficulty";
+
+	public static void SetMasterVolume(float volume)
+	{
+		if (volume > 0f && volume <= 1f)
+		{
+			PlayerPrefs.SetFloat(MasterVolumeKey, volume);
+		}
+		else
+		{
+			Debug.LogError("Volume out of range.");
+		}
+	}
+
+	public static void SetDifficulty(int difficulty)
+	{
+		if (difficulty >= 0 && difficulty <= 2)
+		{
+			PlayerPrefs.SetInt(DifficultyKey, difficulty);
+		}
+		else
+		{
+			Debug.LogError("Difficult out of range.");
+		}
+		
+	}
+
+	public static float GetMasterVolume()
+	{
+		return PlayerPrefs.GetFloat(MasterVolumeKey);
+	}
+}
