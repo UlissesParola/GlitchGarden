313107a6e9b753c68110f2f2a0bf3edae192a270
Ulisses Parola <ulissesmt@gmail.com>
2017-12-12 09:13:23 -0200

    Change the way projectiles Parent is instantiated and associated to the projectiles

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
-
-	public GameObject Projectile, ProjectileParent;
+	public GameObject Projectile; 
 	public Transform Gun;
 	
+	private GameObject _projectileParent;
+
+	private void Start()
+	{
+		_projectileParent = GameObject.Find("Projectiles");
+
+		if (_projectileParent== null)
+		{
+			_projectileParent = new GameObject("Projectiles");
+		}
+	}
+
 	private void FireGun()
 	{
 		GameObject newProjectile = Instantiate(Projectile, Gun);
-		newProjectile.transform.SetParent(ProjectileParent.transform);
+		newProjectile.transform.SetParent(_projectileParent.transform);
 	}
 }
