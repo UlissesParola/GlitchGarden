4d3efd4e15424a989dad7f20586777863b3abc98
Ulisses Parola <ulissesmt@gmail.com>
2017-12-24 09:51:44 -0200

    timer and level transition

---
+++ Assets/Scripts/GameTimer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour
+{
+	public float LevelDuration;
+
+	private LevelManager _levelManager;
+	private float _endTime;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_endTime = Time.time + LevelDuration;
+		_levelManager = FindObjectOfType<LevelManager>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		GetComponent<Slider>().value = Time.time / _endTime;
+		
+		if (Time.time > _endTime)
+		{
+			Debug.Log("Level ended");
+			LevelEnd();
+		}
+	}
+
+	void LevelEnd()
+	{
+		Invoke("AutoLoadNextLevel", 3);
+	}
+
+	void AutoLoadNextLevel()
+	{
+		_levelManager.LoadNextScene();
+	}
+}
